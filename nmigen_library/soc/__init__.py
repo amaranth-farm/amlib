be6b6f7ce7c236648107717517a575044103f2c4
Hans Baier <hansfbaier@gmail.com>
2021-06-26 16:24:59 +0700

    adopt a simple soc generation framework from the LUNA project. Thank Scott!

---
+++ nmigen_library/soc/__init__.py
@@ -0,0 +1,8 @@
+#
+# This file has been adopted from the LUNA project
+#
+# Copyright (c) 2020 Great Scott Gadgets <info@greatscottgadgets.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
+from .simplesoc import SimpleSoC
+from .uart      import UARTPeripheral
