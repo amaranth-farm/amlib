994ec058827f19b9e6ddf0eef4a25ead63c6af42
Hans Baier <hansfbaier@gmail.com>
2021-08-02 07:44:42 +0700

    connect stream to fifo methods: add docstrings

--- nmigen_library/stream/fifo.py
+++ nmigen_library/stream/fifo.py
@@ -8,6 +8,8 @@ from nmigen.lib.fifo import FIFOInterface
 from .               import StreamInterface
 
 def connect_fifo_to_stream(fifo: FIFOInterface, stream: StreamInterface) -> None:
+    """Connects the output of the FIFO to the of the stream. Data flows from the fifo the stream."""
+
     return [
         stream.valid.eq(fifo.r_rdy & fifo.r_en),
         fifo.r_en.eq(stream.ready),
@@ -15,6 +17,8 @@ def connect_fifo_to_stream(fifo: FIFOInterface, stream: StreamInterface) -> None
     ]
 
 def connect_stream_to_fifo(stream: StreamInterface, fifo: FIFOInterface) -> None:
+    """Connects the stream to the input of the FIFO. Data flows from the stream to the FIFO."""
+
     return [
         fifo.w_en.eq(stream.valid),
         stream.ready.eq(fifo.w_rdy),
