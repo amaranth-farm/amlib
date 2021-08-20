619582de2b5fb6a8447d46c6f6643caa8212212d
Hans Baier <hansfbaier@gmail.com>
2021-08-21 06:42:48 +0700

    timer: fix const load/reload and add tests for those

--- nmigen_library/utils/timer.py
+++ nmigen_library/utils/timer.py
@@ -5,8 +5,8 @@ from ..test       import GatewareTestCase, sync_test_case
 class Timer(Elaboratable):
     def __init__(self, *, width=32, load=None, reload=None):
         self._width = width
-        self.load_in     = Signal(width, name="load")   if load   == None else Const(load, width)
-        self.reload_in   = Signal(width, name="reload") if reload == None else Const(reload, width)
+        self.load_in     = Signal(width, name="load")   if load   == None else load
+        self.reload_in   = Signal(width, name="reload") if reload == None else reload
         self.counter_out = Signal(width)
         self.start       = Signal()
         self.done        = Signal()
@@ -106,6 +106,86 @@ class TimerTest(GatewareTestCase):
         yield from self.shouldBeLow(dut.done)
 
         yield from self.advance_cycles(1)
+        self.assertEquals((yield dut.counter_out), 1)
+        yield from self.shouldBeLow(dut.done)
+
+        # counter is zero, transition to DONE
+        yield
+        yield from self.shouldBeHigh(dut.done)
+        yield from self.shouldBeZero(dut.counter_out)
+
+        for _ in range(4):
+            for _ in range(2):
+                yield
+                yield from self.shouldBeLow(dut.done)
+                yield from self.shouldBeNonZero(dut.counter_out)
+
+            yield
+            yield from self.shouldBeZero(dut.counter_out)
+            yield from self.shouldBeHigh(dut.done)
+
+class TimerConstReloadTest(GatewareTestCase):
+    FRAGMENT_UNDER_TEST = Timer
+    FRAGMENT_ARGUMENTS = {'width': 32, 'reload': 0}
+
+    @sync_test_case
+    def test_oneshot(self):
+        dut = self.dut
+        yield
+
+        # load initial value
+        yield dut.load_in.eq(10)
+        yield
+        yield from self.shouldBeLow(dut.done)
+        yield from self.shouldBeZero(dut.counter_out)
+
+        # start timer
+        yield dut.load_in.eq(0)
+        yield from self.pulse(dut.start, step_after=True)
+        self.assertEqual((yield dut.counter_out), 9)
+        yield from self.shouldBeLow(dut.done)
+
+        yield from self.advance_cycles(8)
+        self.assertEqual((yield dut.counter_out), 1)
+        yield from self.shouldBeLow(dut.done)
+
+        # counter is zero, transition to DONE
+        yield
+        yield from self.shouldBeHigh(dut.done)
+        yield from self.shouldBeZero(dut.counter_out)
+
+        for _ in range(5):
+            yield
+            yield from self.shouldBeLow(dut.done)
+            yield from self.shouldBeZero(dut.counter_out)
+
+        yield from self.shouldBeZero(dut.counter_out)
+
+class TimerConstLoadTest(GatewareTestCase):
+    FRAGMENT_UNDER_TEST = Timer
+    FRAGMENT_ARGUMENTS = {'width': 32, 'load': 5}
+
+    @sync_test_case
+    def test_periodic(self):
+        dut = self.dut
+        yield
+        # constant load is automatic
+        self.assertEqual((yield dut.counter_out), 5)
+
+        # load initial value
+        yield dut.reload_in.eq(3)
+        yield
+        yield from self.shouldBeLow(dut.done)
+        self.assertEqual((yield dut.counter_out), 5)
+        yield
+        yield
+
+        # start timer
+        yield from self.pulse(dut.start, step_after=True)
+        self.assertEqual((yield dut.counter_out), 4)
+        yield from self.shouldBeLow(dut.done)
+
+        yield from self.advance_cycles(3)
         self.assertEquals((yield dut.counter_out), 1)
         yield from self.shouldBeLow(dut.done)
 
