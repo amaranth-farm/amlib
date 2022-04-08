3a6aad968d54f36472cae06fdcf39ed0e67c2b52
Rouven Broszeit <roubro1991@gmx.de>
2022-04-08 22:40:25 +0200

    New module for stereo and crossfeed convolution
    This module uses parallel DSPs in order to fit the needed MAC
    calculations for large impulse response in real-time processing.
    The convolution can be done in three modes: mono, stereo and crossfeed.

--- amlib/dsp/__init__.py
+++ amlib/dsp/__init__.py
@@ -1,8 +1,9 @@
-from .resampler           import FractionalResampler
-from .fixedpointiirfilter import FixedPointIIRFilter
-from .fixedpointfirfilter import FixedPointFIRFilter
-from .filterbank          import Filterbank
-from .fixedpointcicfilter import FixedPointCICFilter
-from .fixedpointhbfilter  import FixedPointHBFilter
+from .resampler               import FractionalResampler
+from .fixedpointiirfilter     import FixedPointIIRFilter
+from .fixedpointfirfilter     import FixedPointFIRFilter
+from .filterbank              import Filterbank
+from .fixedpointcicfilter     import FixedPointCICFilter
+from .fixedpointhbfilter      import FixedPointHBFilter
+from .stereo_convolution_mac  import StereoConvolutionMAC, ConvolutionMode
 
-__all__ = [FractionalResampler, FixedPointIIRFilter, FixedPointFIRFilter, Filterbank, FixedPointCICFilter, FixedPointHBFilter]
+__all__ = [FractionalResampler, FixedPointIIRFilter, FixedPointFIRFilter, Filterbank, FixedPointCICFilter, FixedPointHBFilter, StereoConvolutionMAC, ConvolutionMode]
