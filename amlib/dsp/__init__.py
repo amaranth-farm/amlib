0c2372c7161b4b1a42b4888f7d88f0378ce5652e
Rouven Broszeit <roubro1991@gmx.de>
2022-04-11 09:23:32 +0200

    renamed stereo_convolution_mac to new package amlib.dsp.convolution.mac

--- amlib/dsp/__init__.py
+++ amlib/dsp/__init__.py
@@ -4,6 +4,5 @@ from .fixedpointfirfilter     import FixedPointFIRFilter
 from .filterbank              import Filterbank
 from .fixedpointcicfilter     import FixedPointCICFilter
 from .fixedpointhbfilter      import FixedPointHBFilter
-from .stereo_convolution_mac  import StereoConvolutionMAC, ConvolutionMode
 
-__all__ = [FractionalResampler, FixedPointIIRFilter, FixedPointFIRFilter, Filterbank, FixedPointCICFilter, FixedPointHBFilter, StereoConvolutionMAC, ConvolutionMode]
+__all__ = [FractionalResampler, FixedPointIIRFilter, FixedPointFIRFilter, Filterbank, FixedPointCICFilter, FixedPointHBFilter]
